800dbbe998ff0bef3b41b8da7046696d6bdb34e9
marianna kalliostra <marianna kalliostra>
2016-07-31 14:04:15 +0300

    Added new function

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os, time, sys
 
 def Main():
     while True:
-        time_type = input('What type of time do you want to use?\n"h" for hours\n"m" for minutes\n"s" for seconds\n')
-        if time_type == "h" or time_type == "m" or time_type == "s":
+        time_type = input('What type of time do you want to use?\n"h" for hours\n"m" for minutes\n"s" for seconds\n"hm" for Hours:Minutes (eg: 3:43)\n')
+        if time_type == "h" or time_type == "m" or time_type == "s" or time_type == "hm":
             while True:
                 if time_type == "h":
                     try:
@@ -42,6 +42,37 @@ def Main():
                         sys.exit(0)
                     except ValueError:
                         print("Please enter a number of seconds!")
+                if time_type == "hm":
+                    timen = input("In how many Hours:Minutes do you want your computer to shut down?\n")
+                    if len(timen) <= 5 and timen.count(':') == 1:
+                        hours, minutes = timen.split(':')
+                        try:
+                            hours = int(hours)
+                            minutes = int(minutes)
+                            if hours <= 23 and minutes <= 59:
+                                if minutes != 0:
+                                    shutm = minutes * 60
+                                else:
+                                    shutm = 0
+                                if hours != 0:
+                                    shuth = hours * 3600
+                                else:
+                                    shuth = 0
+                                shut = shutm + shuth
+                                print("Please don't close this window!\n")
+                                time.sleep(shut)
+                                print("Shutting down...\nIf you've changed your mind you have 7 seconds to close this window before it shuts your computer down")
+                                time.sleep(7)
+                                os.system('shutdown -s')
+                                sys.exit(0)
+                            else:
+                                print("Maximum is 23:59")
+                        except ValueError:
+                            print("Please make sure you input actual time!")
+                    else:
+                        print("Please make sure you input actual time!")
+                        
+
         else:
             print("Please try again!\n\n")
 
